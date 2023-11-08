c4864b23805f6ecf4351d19ee1a16a597e77b646
SilasMedina <74703755+SilasMedina@users.noreply.github.com>
2023-11-07 23:39:33 -0300

    Arrays em PHP

---
+++ listas.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+
+  $compras = ["banana", "mamão"];
+  echo $compras[0];
+  $moedas = [];
+  $moedas[0] = 0;
+  $moedas[1] = 1;
+  echo $moedas[1];
+  ?>
+</body>
+</html>
