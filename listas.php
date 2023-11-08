6995ae80d34359078ae0418099eea46fdd12b5d6
SilasMedina <74703755+SilasMedina@users.noreply.github.com>
2023-11-07 23:55:14 -0300

    Update listas.php
    Ambas as abordagens funcionarão em versões mais antigas e nas versões mais recentes do PHP.

--- listas.php
+++ listas.php
@@ -10,10 +10,15 @@
 
   $compras = ["banana", "mamão"];
   echo $compras[0];
-  $moedas = [];
-  $moedas[0] = 0;
-  $moedas[1] = 1;
-  echo $moedas[1];
+  $moedas = [];  // Cria um array vazio
+$moedas[0] = 0; // Define o valor para o índice 0
+$moedas[1] = 1; // Define o valor para o índice 1
+
+echo $moedas[1]; // Isso irá imprimir 1, que é o valor associado ao índice
+#Na versão 8 e posteriores do PHP, você também pode usar a sintaxe de inicialização de array para criar um array associativo:
+$moedas2 = [0 => 0, 1 => 1];
+echo $moedas2[1]; // Isso também imprimirá 1
+
   ?>
 </body>
 </html>
