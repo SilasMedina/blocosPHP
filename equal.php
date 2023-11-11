c751f59ed18553edb18b61c912680a78a8ca5b29
SilasMedina <74703755+SilasMedina@users.noreply.github.com>
2023-11-11 01:54:50 -0300

    Create equal.php
    Comparison operator ==

---
+++ equal.php
@@ -0,0 +1,9 @@
+$a = 5;    // aqui temos um valor inteiro atribuído
+$b = "5";  // e aqui temos um valor string
+
+// Operador de comparação ==
+if ($a == $b) {
+    echo "Usando ==: Os valores são iguais.";
+} else {
+    echo "Usando ==: Os valores não são iguais.";
+}
